6a92d129028a68fdcb8b6ecd6ef3405599bfe5c1
kevin940726 <b01705041@ntu.edu.tw>
2014-11-27 17:12:30 +0800

    Initial Commit

---
+++ cplusplus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cplusplus
+//
+//  Created by Kai on 2014/11/27.
+//  Copyright (c) 2014年 Kai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
